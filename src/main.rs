8b94caeafd5a2d47bc9d7a7ae483178563113d0e
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-12-13 16:52:54 -0800

    Easy stuff so far. Basic CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-fn calc_weight_on_mars(weight: &f32) -> f32 {
-    let weight_on_mars: f32 = (weight / 9.81) * 3.711;
-    return weight_on_mars;
-}
+use std::io;
 
 fn main() {
-    let weight_on_earth: f32 = 187.6;
-    let weight_on_mars: f32 = calc_weight_on_mars(&weight_on_earth);
-    // used cargo expand to see macros in action.
-    println!(
-        "My weight on Earch is {}lbs.\nAnd my weight on Mars is {}kg",
-        weight_on_earth, weight_on_mars
-    )
+    print!("Enter your weight (Kg): ");
+    let mut input = String::new();
+
+    // get input from user
+    io::stdin().read_line(&mut input).unwrap();
+
+    // convert it to float using the parse function and then unwrap (unsafely)
+    let weight: f32 = input.trim().parse().unwrap();
+
+    // pass the weight to the calc function we created
+    let mars_weight = calculate_weight_on_mars(weight);
+    println!("Weight on mars is: {}", mars_weight);
+    println!("Weight is {}", weight); // this will work beacause numbers are easily copyable. They implement the 'copy' trait.
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    return (weight / 9.81) * 3.711;
 }
