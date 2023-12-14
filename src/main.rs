bf985286f33b4f6156633d1176fc51e4cf4f7a4b
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-12-13 16:36:50 -0800

    Only one mutable reference allowed in a scope.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,17 @@ fn main() {
         "First name is still {} in the main function where it was declared.",
         first_name
     ); // we can still safely use it here!
+    let mut hello_world: String = String::from("Hello");
+    append_world(&mut hello_world);
+    println!("hello world variable is now {}", hello_world);
 }
 
+// immutable references
 fn say_hello(s: &String) {
     println!("Hello there, {}", s);
+}
+
+// we also have mutable references
+fn append_world(first_name: &mut String) {
+    first_name.push_str(", world");
 }
