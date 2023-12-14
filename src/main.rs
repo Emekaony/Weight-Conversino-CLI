0c3816be76979d9659db1afd682f9cc691842eea
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-12-13 16:20:40 -0800

    References are going well.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-fn calc_weight_on_mars(weight: &f32) -> f32 {
-    let weight_on_mars: f32 = (weight / 9.81) * 3.711;
-    return weight_on_mars;
+fn main() {
+    let first_name: String = String::from("Nnaemeka");
+    say_hello(&first_name); // we have passed a reference to first_name here withour transferring ownership
+    println!(
+        "First name is still {} in the main function where it was declared.",
+        first_name
+    ); // we can still safely use it here!
 }
 
-fn main() {
-    let weight_on_earth: f32 = 187.6;
-    let weight_on_mars: f32 = calc_weight_on_mars(&weight_on_earth);
-    // used cargo expand to see macros in action.
-    println!(
-        "My weight on Earch is {}lbs.\nAnd my weight on Mars is {}kg",
-        weight_on_earth, weight_on_mars
-    )
+fn say_hello(s: &String) {
+    println!("Hello there, {}", s);
 }
